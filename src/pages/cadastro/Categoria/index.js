53235eadd1e4a6d8a5d29133d210d4b3b26e3fcb
Rod Andrade <rodrigoandradebcc@gmail.com>
2020-08-07 11:09:14 -0300

    commit heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,7 +30,7 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categoria';
+    const URL_TOP = 'http://localhost:8080/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
