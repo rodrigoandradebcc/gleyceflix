c708aab7ace41b5730c7b0941b8b0f7fb5664139
Rod Andrade <rodrigoandradebcc@gmail.com>
2020-08-07 15:48:52 -0300

    retiando o s

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -18,7 +18,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
-      ? 'https://localhost:8080/categorias'
+      ? 'http://localhost:8080/categorias'
       : 'https://gleyceflix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
