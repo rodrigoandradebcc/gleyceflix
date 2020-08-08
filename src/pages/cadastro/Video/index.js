cc89d8b890bb9be3566868381928a80e7b0a2fe8
Rod Andrade <rodrigoandradebcc@gmail.com>
2020-08-08 01:49:36 -0300

    salvando alterações

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,17 +1,69 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import { Link } from 'react-router-dom';
+import useForm from '../../../hocks/useForm';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
+import videosRepository from '../../../repositories/videos';
 
-function CadastroVideo(){
-    return (
-        <PageDefault>
-            <h1>Cadastro de Vídeo</h1>
+function CadastroVideo() {
+  const history = useHistory();
+  const { handleChange, values } = useForm({
+    titulo: 'Video Padrão',
+    url: 'https://www.youtube.com/watch?v=ROeA2H-EQ6w',
+    categoria: 'Front End',
+  });
 
-            <Link to="/cadastro/categoria">
-                Cadastrar Categoria
-            </Link>
-        </PageDefault>
-    )
+  return (
+    <PageDefault>
+      <h1>Cadastro de Vídeo</h1>
+
+      <form onSubmit={(event) => {
+        event.preventDefault();
+        // alert('Vídeo Cadastrado com sucesso!!!');
+
+        videosRepository.create({
+          titulo: values.titulo,
+          url: values.url,
+          categoriaId: 1,
+        })
+          .then(() => {
+            console.log('Cadastrou com sucesso');
+            history.push('/');
+          });
+      }}
+      >
+        <FormField
+          label="Título do Vídeo"
+          name="titulo"
+          value={values.titulo}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="URL do Vídeo"
+          name="url"
+          value={values.url}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Categoria"
+          name="url"
+          value={values.categoria}
+          onChange={handleChange}
+        />
+
+        <Button type="submit">
+          Cadastrar
+        </Button>
+      </form>
+
+      <Link to="/cadastro/categoria">
+        Cadastrar Categoria
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroVideo;
